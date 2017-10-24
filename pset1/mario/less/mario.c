ff9965ecfb2cfea954b58c5638f363e000c6e21a
Islam Saffaru <samsoffar@gmail.com>
2017-10-24 19:34:03 +0000

    pset1 mario meets requirements

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -9,15 +9,23 @@ int main (void)
         printf("Height:\n");
         height = get_int();
     }
-    while(height<0 | height>23);
-    
-    for (int i = 2; i < height+2; i++)
+    while (height < 0 | height > 23);
+    //pyramid builder start with two blocks (i=2)
+    for (int i = 2; i < height + 2; i++)
     {
+        //Space builder note the (+1) will give it the right-- 
+        //shape,more visible with bottom leayer. 
+        int spaces = height - i + 1;
+        for (int c = 0; c < spaces; c++)
+        {
+            printf(" ");
+        }
+        //Block builder
         for (int c = 0; c < i; c++)
         {
             printf("#");
-            
         }
+        //Go to a new line after builder one layer
         printf("\n");
     }
 }
