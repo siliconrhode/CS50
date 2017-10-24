30a227d485af136b3f954708e12d04540209d6ea
Islam Saffaru <samsoffar@gmail.com>
2017-10-24 19:49:53 +0000

    better styling

--- pset1/water/water.c
+++ pset1/water/water.c
@@ -4,17 +4,15 @@
 int main(void)
 {
     int minutes;
-    
+    //Minutes: prompt
     do
     {
-    printf("Minutes:");
-    minutes = get_int();
+        printf("Minutes:");
+        minutes = get_int();
     }
-    
-    while (minutes<0);
-    int bottles =minutes * 12;
+    // keep prompting positive input from user
+    while (minutes < 0);
+    //calculate the number of bottles per minute
+    int bottles = minutes * 12;
     printf("Bottles:%i\n", bottles);
-
-    
-    
 }
